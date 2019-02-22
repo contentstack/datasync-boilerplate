6874903fd5636fe4cff4b1eba4976d5f2896af64
ninadhatkar <ninadh22@gmail.com>
2019-02-22 18:11:38 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export const config = {
   contentStoreModule: 'contentstack-content-store-mongodb',
-  assetStoreModule: 'mock-contentstack-asset-store-filesystem',
+  assetStoreModule: 'contentstack-asset-store-filesystem',
   listenerModule: 'contentstack-webhook-listener',
   contentstack: {
     apiKey: '',
