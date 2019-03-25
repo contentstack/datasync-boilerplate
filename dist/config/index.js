c13b35ab4e49f7b4448450b5ef9829e170c9405b
almeida-shrutika <almeidashrutika4497@gmail.com>
2019-03-25 19:21:22 +0530

    Updated README.md and modules

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,9 +1,14 @@
-export const config = {
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var config = exports.config = {
   contentStoreModule: '@contentstack/datasync-content-store-filesystem',
   assetStoreModule: '@contentstack/datasync-asset-store-filesystem',
   listenerModule: '@contentstack/webhook-listener',
   contentstack: {
     apiKey: '',
-    deliveryToken: '',
-  },
-}
+    deliveryToken: ''
+  }
+};
