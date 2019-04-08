e516c2577c7519b59b6b6bbf014f270902ca16a1
asmit-patil <patilasmit0gmail.com>
2019-04-08 19:18:10 +0530

    updated filsystem content store and asset store module

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 export const config = {
-  contentStoreModule: '@contentstack/datasync-content-store-mongodb',
+  contentStoreModule: '@contentstack/datasync-content-store-filesystem',
   assetStoreModule: '@contentstack/datasync-asset-store-filesystem',
   listenerModule: '@contentstack/webhook-listener',
   contentstack: {
-    apiKey: '',
-    deliveryToken: '',
+    apiKey: '***REMOVED***',
+    deliveryToken: '***REMOVED***'
   },
 }
