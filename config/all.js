49f6b0735176b4305bbce203afb5f3ed07046112
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-07-26 13:15:35 +0530

    v1.0.0 pre-release

--- config/all.js
+++ config/all.js
@@ -1,11 +1,67 @@
 const config = {
+  // Make sure you have all the modules mentioned below installed
   contentStoreModule: '@contentstack/datasync-content-store-filesystem',
   assetStoreModule: '@contentstack/datasync-asset-store-filesystem',
   listenerModule: '@contentstack/webhook-listener',
   contentstack: {
+    // Your stack's API key
     apiKey: '',
-    deliveryToken: ''
+    // Your stack's environment based delivery token. (Goto Contentstack Dashboard -> Settings dropdown -> Tokens -> Generate tokens)
+    deliveryToken: '',
   },
+  plugins: [
+    // Plugins starting with _cs_internal_ are app specific internal plugins
+    // If you have custom plugins
+    //  1. Ceate a 'plugins' folder in this app's directory
+    //  2. Create a folder inside it
+    //  3. Inside the folder, create a index.js file
+    // Check example - plugins/dummy-plugin of this project
+    {
+      disabled: true,
+      name: 'item-logger',
+      options: {
+        // Here, you can set your own custom logger
+        log: console.log
+      }
+    },
+    {
+      disabled: false,
+      // This plugin 
+      //  - Modifies reference field structure of Stack's v3.4.x and below to new reference field structure (Array of uids, transformed to Array of objects)
+      //  - Updates all old reference fields and makes them compatible with multiple references
+      name: '_cs_internal_transform_entries',
+      // path: '',
+      options: {
+        // other overrides...
+      },
+    },
+    {
+      disabled: false,
+      // This plugin 
+      //  - Detects asset/reference paths in a content type's schema
+      //  - Updates all content types with asset/reference fields
+      // Which can be later used with datasync-filesystem-sdk OR datasync-mongodb-sdk
+      name: '_cs_internal_transform_schemas',
+      options: {
+        // Set false, if you do not wish to log asset references in the schema
+        logAssetPaths: true,
+        // Set false, if you do not wish to log entry references in the scehma
+        logReferencePaths: true,
+      },
+    },
+    {
+      disabled: false,
+      // This plugin
+      //  - Detects RTE/Markdown fields in a schema
+      //  - Finds all the assets that's stored in Contentstack
+      //  - Downloads all assets onto the asset store specified
+      //  - Updates the entry's RTE/Markdown fields
+      name: '_cs_internal_save_rte_markdown_assets',
+      options: {
+        // other overrides...
+      },
+    },
+  ],
 }
 
 module.exports = config
--- config/development.js
+++ config/development.js
@@ -1,8 +1,11 @@
 const config = {
-  // By default, we'd be using contentstack-content-store-filesystem, the following keys can be configured for it. To use, kindly uncomment it
-  // contentStore: {
-  //     baseDir: './_contents'
-  // }
+  // By default, we'd be using contentstack-content-store-filesystem, the following keys can be configured for it.
+  contentStore: {
+    baseDir: './_development_contents',
+  },
+  assetStore: {
+    baseDir: './_development_contents',
+  }
 }
 
 module.exports = config
