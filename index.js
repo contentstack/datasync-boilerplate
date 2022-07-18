2a72a1287704c660b28f6843b6cae66bd306a47f
Netraj Patel <netraj.patel@netrajpatel.local>
2022-07-18 20:18:42 +0530

    Updated package versions and formatting changes

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-const fs = require('fs')
-const path = require('path')
-const _ = require('lodash')
-const datasyncManager = require('@contentstack/datasync-manager')
-const config = require('./config/all')
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+const datasyncManager = require("@contentstack/datasync-manager");
+const config = require("./config/all");
 /**
  * Listener module. Currently Contentstack offers
  * - @contentstack/webhook-listener
  */
-const listener = require(config.listenerModule)
+const listener = require(config.listenerModule);
 /**
  * Asset store module. Currently Contentstack offers
  * - @contentstack/datasync-asset-store-filesystem
  */
-const assetStore = require(config.assetStoreModule)
+const assetStore = require(config.assetStoreModule);
 /**
  * Content store module. Currently Contentstack offers
  * - @contentstack/datasync-content-store-filesystem (To query contents synced via filesystem, you can use https://github.com/contentstack/datasync-filesystem-sdk)
  * - @contentstack/datasync-content-store-mongodb (To query contents synced via mongodb, you can use https://github.com/contentstack/datasync-mongodb-sdk)
  */
-const contentStore = require(config.contentStoreModule)
+const contentStore = require(config.contentStoreModule);
 
 // Set application's environment
-const env = process.env.NODE_ENV || config.environment || 'development'
+const env = process.env.NODE_ENV || config.environment || "development";
 
-let envConfig
-if (fs.existsSync(path.join(__dirname, 'config', env + '.js'))) {
-  envConfig = require(path.join(__dirname, 'config', env))
+let envConfig;
+if (fs.existsSync(path.join(__dirname, "config", env + ".js"))) {
+  envConfig = require(path.join(__dirname, "config", env));
 } else {
-  envConfig = require(path.join(__dirname, 'config', 'development'))
+  envConfig = require(path.join(__dirname, "config", "development"));
 }
 
-const appConfig = _.merge(config, envConfig)
+const appConfig = _.merge(config, envConfig);
 
-datasyncManager.setConfig(appConfig)
-datasyncManager.setAssetStore(assetStore)
-datasyncManager.setContentStore(contentStore)
-datasyncManager.setListener(listener)
+datasyncManager.setConfig(appConfig);
+datasyncManager.setAssetStore(assetStore);
+datasyncManager.setContentStore(contentStore);
+datasyncManager.setListener(listener);
 
-datasyncManager.start()
+datasyncManager
+  .start()
   .then(() => {
-    console.log('Boilerplate: DataSync started successfully!')
+    console.log("Boilerplate: DataSync started successfully!");
   })
-  .catch(console.error)
-  
+  .catch((error) => {
+    console.error(error);
+  });
